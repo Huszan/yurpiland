a404300dcc6005d10c0f01df6a320c7ab5524e46
Huszan <mateuszjacenty1@gmail.com>
2023-11-03 19:31:22 +0100

    Add modal

--- src/context/GlobalStates.js
+++ src/context/GlobalStates.js
@@ -5,6 +5,19 @@ export const GlobalStatesContext = createContext();
 export default function GlobalStates(props) {
     const { children } = props;
     const [isNavDrawerOpen, setIsNavDrawerOpen] = useState(false);
+    const [modalData, setModalData] = useState({
+        content: <ul>
+            <li>Test1</li>
+            <li>Test2</li>
+            <li>Test3</li>
+            <li>Test4</li>
+            <li>Test5</li>
+            <li>Test6</li>
+            <li>Test7</li>
+            <li>Test8</li>
+        </ul>,
+        isOpen: true,
+    });
 
 
     return (
@@ -12,6 +25,8 @@ export default function GlobalStates(props) {
             value = {{
                 isNavDrawerOpen,
                 setIsNavDrawerOpen,
+                modalData,
+                setModalData,
             }}
         >
             { children }
