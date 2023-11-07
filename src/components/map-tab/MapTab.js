366ec1afe63492bed60b5d441d186b939c2a77ec
Huszan <mateuszjacenty1@gmail.com>
2023-11-07 20:13:15 +0100

    Move map progress to main

--- src/components/map-tab/MapTab.js
+++ src/components/map-tab/MapTab.js
@@ -1,37 +1,12 @@
 import './MapTab.scss';
 import Map from '../map/Map';
-import MapInfo from '../map-info/MapInfo';
-import { useContext, useRef } from 'react';
-import MapProgress from '../map-progress/MapProgress';
-import { ProgressionContext } from '../../context/Progression';
-import { GlobalStatesContext } from '../../context/GlobalStates';
+import { useRef } from 'react';
 
 export default function MapTab() {
     const mapTabRef = useRef();
-    const progress = useContext(ProgressionContext);
-    const globalStates = useContext(GlobalStatesContext);
-    const locations = progress.locations;
-
-    function openInfo() {
-        globalStates.setModalData(prev => {
-            return {
-                ...prev,
-                content: <MapInfo 
-                    location={locations.get.selected}
-                    resources={progress.resources}
-                />,
-                isOpen: true,
-            }
-        })
-    }
 
     return (
         <div id='map-tab' ref={mapTabRef}>
-            <MapProgress 
-                openInfo={() => openInfo()}
-                adventure={progress.adventure}
-                location={locations.get.selected}
-            />
             <Map />
         </div>
     )
