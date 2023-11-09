51cf02ddb3bdcd3078463175ce55a1b6f23cbfe8
Huszan <mateuszjacenty1@gmail.com>
2023-11-09 13:29:45 +0100

    Data encryption

---
+++ src/utils/Encryption.js
@@ -0,0 +1,24 @@
+import CryptoJS from "crypto-js";
+
+export function encrypt(data, key) {
+    const cipher = CryptoJS.AES.encrypt(
+        JSON.stringify(data),
+        key ? key : process.env.REACT_APP_ENCRYPT_KEY,
+    ).toString();
+    return cipher;
+}
+
+export function decrypt(cipher, key) {
+    const bytes = CryptoJS.AES.decrypt(
+        cipher,
+        key ? key : process.env.REACT_APP_ENCRYPT_KEY,
+    );
+    try {
+        const data = JSON.parse(
+            bytes.toString(CryptoJS.enc.Utf8)
+        );
+        return data;
+    } catch (e) {
+        return null;
+    }
+}
