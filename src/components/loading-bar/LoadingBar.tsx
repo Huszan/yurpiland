1818425d7dd0a2b0bc86ef74380c6284cc2e40f5
Mateusz Jacenty <mateuszjacenty1@gmail.com>
2024-12-11 11:18:17 +0100

    Rework LoadingBar

--- src/components/loading-bar/LoadingBar.tsx
+++ src/components/loading-bar/LoadingBar.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useMemo } from "react";
 import { formatTime } from "../../utils/HelperFunctions.utils";
 import "./LoadingBar.scss";
 
@@ -16,30 +16,24 @@ export type LoadingBarComponentConfig = {
 
 export default function LoadingBar(props: LoadingBarComponentProps) {
     const { progress, timeLeft, config } = props;
-    const barRef = useRef<HTMLDivElement | null>(null);
-    const fillRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        if (!barRef.current || !fillRef.current) return;
-        barRef.current.style.backgroundImage = config.barBg
-            ? `url(${config.barBg})`
-            : "unset";
-        barRef.current.style.backgroundColor = config.barBgColor
-            ? config.barBgColor
-            : "unset";
-        fillRef.current.style.backgroundColor = config.fillBgColor
-            ? config.fillBgColor
-            : "unset";
+    const barStyle: React.CSSProperties = useMemo(() => {
+        return {
+            backgroundImage: config.barBg ? `url(${config.barBg})` : "unset",
+            backgroundColor: config.barBgColor ? config.barBgColor : "unset",
+        };
     }, [config]);
 
-    useEffect(() => {
-        if (!fillRef.current) return;
-        fillRef.current.style.width = `${progress ? progress : 0}%`;
-    }, [progress]);
+    const fillStyle: React.CSSProperties = useMemo(() => {
+        return {
+            backgroundColor: config.fillBgColor ? config.fillBgColor : "unset",
+            width: `${progress ? progress : 0}%`,
+        };
+    }, [config, progress]);
 
     return (
-        <div className="loading-bar" ref={barRef}>
-            <div className="fill" ref={fillRef}></div>
+        <div className="loading-bar" style={barStyle}>
+            <div className="fill" style={fillStyle}></div>
             <div className="progress-indicator"></div>
             {config.barBg && <div className="bg-filter"></div>}
             <span className="time-left center-abs">
